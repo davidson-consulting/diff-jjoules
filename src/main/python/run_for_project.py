9000da1a3682c8b82e7ba24bbb7b1beb4f330796
danglotb <benjamin.danglot@davidson.fr>
2020-12-12 11:01:11 +0100

    fix: do not delete in case of error, we can track down the errors now

--- src/main/python/run_for_project.py
+++ src/main/python/run_for_project.py
@@ -106,8 +106,6 @@ if __name__ == '__main__':
             current_nb_completed_commits = current_nb_completed_commits + 1
             print_to_file('Success!' + str(current_nb_completed_commits) + ' / ' + str(nb_commits), current_output_path_log)
             print('Success!', current_nb_completed_commits, '/', nb_commits)
-        else:
-            delete_directory(current_output_path)
-        print_to_file(str(cursor_commits) + ' / ' + str(len(commits) - 1), current_output_path_log)
         print(cursor_commits, '/', len(commits) - 1)
+        print_to_file(str(cursor_commits) + ' / ' + str(len(commits) - 1), current_output_path_log)
         cursor_commits = cursor_commits + 1
