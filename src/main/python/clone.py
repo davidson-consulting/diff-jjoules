05d34b9c656f606e6ef4720170230dddf80bf57b
danglotb <benjamin.danglot@davidson.fr>
2020-12-16 10:30:57 +0100

    remove code for exp

--- src/main/python/clone.py
+++
@@ -1,9 +0,0 @@
-from utils.cmd_utils import *
-from run import *
-import sys
-
-if __name__ == '__main__':
-
-    repo_url = sys.argv[1]
-    clone(repo_url, PATH_V1)
-    clone(repo_url, PATH_V2)
--- src/main/python/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .run_args import *
-from .cmd_utils import *
-from .data_args import *
-from .graph_args import *
-from .json_utils import *
-
-__version__ = '0.0.1'
