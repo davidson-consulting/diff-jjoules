f42cedaf775ac557741b9973d60c52e6417fcfaa
danglotb <benjamin.danglot@davidson.fr>
2021-06-24 19:12:58 +0200

    refactor: rename classpath to classpath V1

--- src/main/java/fr/davidson/diff/jjoules/instrumentation/configuration/Configuration.java
+++ src/main/java/fr/davidson/diff/jjoules/instrumentation/configuration/Configuration.java
@@ -16,8 +16,6 @@ public class Configuration {
 
     public final String pathToTestListAsCSV;
 
-    public final String[] classpath;
-
     public final String[] classpathV1;
 
     public final String[] classpathV2;
@@ -27,15 +25,14 @@ public class Configuration {
     public Configuration(String pathToFirstVersion,
                          String pathToSecondVersion,
                          String pathToTestListAsCSV,
-                         String[] classpath,
+                         String[] classpathV1,
                          String[] classpathV2,
                          boolean junit4) {
         this.pathToFirstVersion = pathToFirstVersion;
         this.pathToSecondVersion = pathToSecondVersion;
         this.pathToTestListAsCSV = pathToTestListAsCSV == null || pathToTestListAsCSV.isEmpty() ? "" : new File(pathToTestListAsCSV).isAbsolute() ? pathToTestListAsCSV : this.pathToFirstVersion + "/" + pathToTestListAsCSV;
         this.junit4 = junit4;
-        this.classpath = classpath;
-        this.classpathV1 = classpath;
+        this.classpathV1 = classpathV1;
         this.classpathV2 = classpathV2;
     }
 
@@ -45,7 +42,8 @@ public class Configuration {
                 "pathToFirstVersion='" + pathToFirstVersion + '\'' +
                 ", pathToSecondVersion='" + pathToSecondVersion + '\'' +
                 ", pathToTestListAsCSV='" + pathToTestListAsCSV + '\'' +
-                ", classpath=" + Arrays.toString(classpath) +
+                ", classpathV1=" + Arrays.toString(classpathV1) +
+                ", classpathV2=" + Arrays.toString(classpathV2) +
                 ", junit4=" + junit4 +
                 '}';
     }
