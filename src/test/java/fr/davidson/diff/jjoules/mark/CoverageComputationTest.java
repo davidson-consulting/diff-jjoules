60df4a9eab682042478c2835af573e8dddbd51f3
danglotb <benjamin.danglot@davidson.fr>
2021-09-30 16:00:18 +0200

    test: add test for CoverageComputation

---
+++ src/test/java/fr/davidson/diff/jjoules/mark/CoverageComputationTest.java
@@ -0,0 +1,45 @@
+package fr.davidson.diff.jjoules.mark;
+
+import eu.stamp_project.diff_test_selection.coverage.Coverage;
+import fr.davidson.diff.jjoules.mark.computation.CoverageComputation;
+import fr.davidson.diff.jjoules.util.JSONUtils;
+import fr.davidson.diff.jjoules.util.Utils;
+import fr.davidson.diff.jjoules.util.maven.MavenRunner;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * on 30/09/2021
+ */
+public class CoverageComputationTest {
+
+    @Test
+    void test() {
+        final String absolutePath = new File("src/test/resources/diff-jjoules-demo").getAbsolutePath();
+        final String testClassName = "fr.davidson.diff_jjoules_demo.InternalListTest";
+        final List<String> testMethodNames = Arrays.asList(
+                "testMapEmptyList",
+                "testMapOneElement",
+                "testMapMultipleElement",
+                "testCount",
+                "testCount2"
+        );
+        final Coverage actual = CoverageComputation.computeCoverageForGivenVersionOfTests(
+                new HashMap<String, List<String>>() {
+                    {
+                        put(testClassName, new ArrayList<>());
+                        get(testClassName).addAll(testMethodNames);
+                    }
+                }, absolutePath
+        );
+        final Coverage expected = JSONUtils.read(absolutePath + "/coverage.json", Coverage.class);
+        assertEquals(expected.toString(), actual.toString());
+    }
+}
