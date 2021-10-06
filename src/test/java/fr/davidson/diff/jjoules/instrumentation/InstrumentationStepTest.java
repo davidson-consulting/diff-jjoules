609630e2504b5dff2aad1f365ae11380318727e4
danglotb <benjamin.danglot@davidson.fr>
2021-10-06 17:06:52 +0200

    test: add test for InstrumentationStep

--- src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationStepTest.java
+++ src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationStepTest.java
@@ -1,4 +1,4 @@
-package fr.davidson.diff.jjoules.failer;
+package fr.davidson.diff.jjoules.instrumentation;
 
 import fr.davidson.diff.jjoules.AbstractStepCleanTest;
 import fr.davidson.diff.jjoules.util.Utils;
@@ -8,36 +8,34 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Benjamin DANGLOT
  * benjamin.danglot@davidson.fr
  * on 06/10/2021
  */
-public class FailerStepTest extends AbstractStepCleanTest {
+public class InstrumentationStepTest extends AbstractStepCleanTest {
 
     @Test
     void test() throws Exception {
         /*
-            After applying the FailerStep, the test should contains 6 calls to junit.framework.Assert.fail();
+
          */
-        final FailerStep failerStep = new FailerStep();
+        final InstrumentationStep instrumentationStep = new InstrumentationStep();
         try (BufferedReader reader = new BufferedReader(new FileReader(ROOT_PATH_V1 + Utils.TEST_FOLDER_PATH + TEST_CLASS_PATH))) {
-            assertFalse(reader.lines().collect(Collectors.joining("\n")).contains("junit.framework.Assert.fail();"));
+            assertFalse(reader.lines().collect(Collectors.joining("\n")).contains("@EnergyTest"));
         }
         try (BufferedReader reader = new BufferedReader(new FileReader(ROOT_PATH_V2 + Utils.TEST_FOLDER_PATH + TEST_CLASS_PATH))) {
-            assertFalse(reader.lines().collect(Collectors.joining("\n")).contains("junit.framework.Assert.fail();"));
+            assertFalse(reader.lines().collect(Collectors.joining("\n")).contains("@EnergyTest"));
         }
-        failerStep.run(this.getConfiguration());
+        instrumentationStep.run(this.getConfiguration());
         try (BufferedReader reader = new BufferedReader(new FileReader(ROOT_PATH_V1 + Utils.TEST_FOLDER_PATH + TEST_CLASS_PATH))) {
-            assertEquals(6L, reader.lines().filter(line -> line.contains("junit.framework.Assert.fail();")).count());
+            assertTrue(reader.lines().collect(Collectors.joining("\n")).contains("@EnergyTest"));
         }
         try (BufferedReader reader = new BufferedReader(new FileReader(ROOT_PATH_V2 + Utils.TEST_FOLDER_PATH + TEST_CLASS_PATH))) {
-            assertEquals(6L, reader.lines().filter(line -> line.contains("junit.framework.Assert.fail();")).count());
+            assertTrue(reader.lines().collect(Collectors.joining("\n")).contains("@EnergyTest"));
         }
     }
-
-
 }
