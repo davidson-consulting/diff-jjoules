0c9925ce03961ad21db0e86ed17cbea5f45d3d09
danglotb <benjamin.danglot@davidson.fr>
2021-10-07 12:50:57 +0200

    test: add test description

--- src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationStepTest.java
+++ src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationStepTest.java
@@ -21,7 +21,7 @@ public class InstrumentationStepTest extends AbstractStepCleanTest {
     @Test
     void test() throws Exception {
         /*
-
+            After the Instrumentation Step, the test class should contains the JUnit-JJoules annotation: @EnergyTest
          */
         final InstrumentationStep instrumentationStep = new InstrumentationStep();
         try (BufferedReader reader = new BufferedReader(new FileReader(ROOT_PATH_V1 + Utils.TEST_FOLDER_PATH + TEST_CLASS_PATH))) {
