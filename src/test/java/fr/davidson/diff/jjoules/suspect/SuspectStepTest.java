0a28f30ba753b8a23087084bd70409b87cf81c13
danglotb <benjamin.danglot@davidson.fr>
2021-10-08 10:55:41 +0200

    test: add test for SuspectTest

---
+++ src/test/java/fr/davidson/diff/jjoules/suspect/SuspectStepTest.java
@@ -0,0 +1,44 @@
+package fr.davidson.diff.jjoules.suspect;
+
+import fr.davidson.diff.jjoules.AbstractDiffJJoulesStepTest;
+import fr.davidson.diff.jjoules.Configuration;
+import fr.davidson.diff.jjoules.mark.computation.ExecsLines;
+import fr.davidson.diff.jjoules.util.JSONUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * on 08/10/2021
+ */
+public class SuspectStepTest extends AbstractDiffJJoulesStepTest {
+
+    @Test
+    void test() {
+        final Configuration configuration = this.getConfiguration();
+        final ExecsLines execDeletions = JSONUtils.read("src/test/resources/diff-jjoules-demo/exec_deletions.json", ExecsLines.class);
+        final ExecsLines execAdditions = JSONUtils.read("src/test/resources/diff-jjoules-demo-v2/exec_additions.json", ExecsLines.class);
+        configuration.setExecLinesDeletions(execDeletions);
+        configuration.setExecLinesAdditions(execAdditions);
+        configuration.setTestsList(
+                new HashMap<String, List<String>>() {
+                    {
+                        put(FULL_QUALIFIED_NAME_TEST_CLASS, new ArrayList<>());
+                        get(FULL_QUALIFIED_NAME_TEST_CLASS).addAll(
+                                Arrays.asList("testCount", "testCount2Failing")
+                        );
+                    }
+                }
+        );
+        assertTrue(configuration.getScorePerLineV1().isEmpty());
+        assertTrue(configuration.getScorePerLineV2().isEmpty());
+        new SuspectStep()._run(configuration);
+        assertTrue(configuration.getScorePerLineV1().isEmpty());
+        assertFalse(configuration.getScorePerLineV2().isEmpty());
+    }
+}
