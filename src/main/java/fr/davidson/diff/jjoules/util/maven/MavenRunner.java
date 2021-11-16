1d17e5b9c355498bf725cfb47fe828ca1d8b7d8b
danglotb <benjamin.danglot@davidson.fr>
2021-11-16 11:40:16 +0100

    feat: compute the maven home path from environnment variables

--- src/main/java/fr/davidson/diff/jjoules/util/maven/MavenRunner.java
+++ src/main/java/fr/davidson/diff/jjoules/util/maven/MavenRunner.java
@@ -6,7 +6,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -55,12 +58,45 @@ public class MavenRunner {
                         .collect(Collectors.joining(" "))
         );
         final Invoker invoker = new DefaultInvoker();
-        invoker.setMavenHome(new File("/usr/share/maven"));
+        if (mavenHome == null) {
+            setMavenHome();
+        }
+        invoker.setMavenHome(new File(mavenHome));
         try {
             final InvocationResult invocationResult = invoker.execute(invocationRequest);
         } catch (MavenInvocationException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String mavenHome;
+
+    private static void setMavenHome() {
+        LOGGER.warn("Trying to lookup for maven home.");
+        LOGGER.warn("This can fail, and thus lead to a crash of the application.");
+        LOGGER.warn("You can set this value using the field mavenHome or defining the following property: MAVEN_HOME or M2_HOME");
+        mavenHome = getMavenHome(envVariable -> System.getenv().get(envVariable) != null,
+                envVariable -> System.getenv().get(envVariable),
+                "MAVEN_HOME", "M2_HOME");
+        if (mavenHome == null) {
+            mavenHome = getMavenHome(path -> new File(path).exists(),
+                    Function.identity(),
+                    "/usr/share/maven/", "/usr/local/maven-3.3.9/", "/usr/share/maven3/", "/usr/share/apache-maven-3.8.1");
+            if (mavenHome == null) {
+                throw new RuntimeException("Maven home not found, please set properly MAVEN_HOME or M2_HOME.");
+            }
+        }
+    }
+
+    private static String getMavenHome(Predicate<String> conditional,
+                                       Function<String, String> getFunction,
+                                       String... possibleValues) {
+        String mavenHome = null;
+        final Optional<String> potentialMavenHome = Arrays.stream(possibleValues).filter(conditional).findFirst();
+        if (potentialMavenHome.isPresent()) {
+            mavenHome = getFunction.apply(potentialMavenHome.get());
+        }
+        return mavenHome;
     }
 
 }
