993aa00d726be4226a5af9999e1003136ec15d11
danglotb <benjamin.danglot@davidson.fr>
2021-11-22 11:44:03 +0100

    refactor: selection does not use OpenClover but rather test-runner

--- src/main/java/fr/davidson/diff/jjoules/selection/SelectionStep.java
+++ src/main/java/fr/davidson/diff/jjoules/selection/SelectionStep.java
@@ -5,9 +5,12 @@ import eu.stamp_project.diff_test_selection.coverage.DiffCoverage;
 import eu.stamp_project.diff_test_selection.selector.EnhancedDiffTestSelection;
 import fr.davidson.diff.jjoules.Configuration;
 import fr.davidson.diff.jjoules.DiffJJoulesStep;
+import fr.davidson.diff.jjoules.selection.detection.TestDetector;
+import fr.davidson.diff.jjoules.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -30,18 +33,27 @@ public class SelectionStep extends DiffJJoulesStep {
     protected void _run(Configuration configuration) {
         this.configuration = configuration;
         LOGGER.info("Run Selection");
+        final List<String> allFullQualifiedNameTestClassesV1 = new TestDetector(
+                configuration.pathToFirstVersion + "/" + Utils.TEST_FOLDER_PATH
+        ).getAllFullQualifiedNameTestClasses();
+        final List<String> allFullQualifiedNameTestClassesV2 = new TestDetector(
+                configuration.pathToSecondVersion + "/" + Utils.TEST_FOLDER_PATH
+        ).getAllFullQualifiedNameTestClasses();
+
         final Coverage coverageV1 = CoverageComputation.convert(
                 CoverageComputation.getCoverage(
-                    this.configuration.pathToFirstVersion,
-                    this.configuration.getClasspathV1AsString(),
-                    this.configuration.junit4
+                        this.configuration.pathToFirstVersion,
+                        this.configuration.getClasspathV1AsString(),
+                        this.configuration.junit4,
+                        allFullQualifiedNameTestClassesV1
                 )
         );
         final Coverage coverageV2 = CoverageComputation.convert(
                 CoverageComputation.getCoverage(
                         this.configuration.pathToSecondVersion,
                         this.configuration.getClasspathV2AsString(),
-                        this.configuration.junit4
+                        this.configuration.junit4,
+                        allFullQualifiedNameTestClassesV2
                 )
         );
         final DiffCoverage coverage = new DiffCoverage();
@@ -56,7 +68,6 @@ public class SelectionStep extends DiffJJoulesStep {
         final Map<String, Set<String>> testsList = enhancedDiffTestSelection.selectTests();
         this.configuration.setTestsList(testsList);
     }
-
 
 
 }
--- src/test/java/fr/davidson/diff/jjoules/selection/SelectionStepTest.java
+++ src/test/java/fr/davidson/diff/jjoules/selection/SelectionStepTest.java
@@ -1,7 +1,11 @@
 package fr.davidson.diff.jjoules.selection;
 
 import fr.davidson.diff.jjoules.AbstractDiffJJoulesStepTest;
+import fr.davidson.diff.jjoules.Configuration;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Benjamin DANGLOT
@@ -12,6 +16,10 @@ public class SelectionStepTest extends AbstractDiffJJoulesStepTest {
 
     @Test
     void test() {
-      // TODO
+        final Configuration configuration = this.getConfiguration();
+        configuration.getTestsList().clear();
+        assertTrue(configuration.getTestsList().isEmpty());
+        new SelectionStep()._run(configuration);
+        assertEquals(6, configuration.getTestsList().get("fr.davidson.diff_jjoules_demo.InternalListTest").size());
     }
 }
