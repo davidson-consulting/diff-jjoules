4ab1a3879c625e267a45c2fbdab2b127a4a8ecda
danglotb <benjamin.danglot@davidson.fr>
2021-11-22 11:35:00 +0100

    feat: add test detection from sources

---
+++ src/main/java/fr/davidson/diff/jjoules/selection/detection/TestDetector.java
@@ -0,0 +1,38 @@
+package fr.davidson.diff.jjoules.selection.detection;
+
+import eu.stamp_project.testrunner.test_framework.TestFramework;
+import spoon.Launcher;
+import spoon.OutputType;
+import spoon.reflect.declaration.CtType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * on 22/11/2021
+ */
+public class TestDetector {
+
+    private Launcher launcher;
+
+    public TestDetector(String pathToTestFolder) {
+        this.launcher = new Launcher();
+        this.launcher.addInputResource(pathToTestFolder);
+        this.launcher.getEnvironment().setNoClasspath(true);
+        this.launcher.getEnvironment().setShouldCompile(false);
+        this.launcher.getEnvironment().setOutputType(OutputType.NO_OUTPUT);
+        this.launcher.buildModel();
+        TestFramework.init(launcher.getFactory());
+    }
+
+    public List<CtType<?>> getAllTestClasses() {
+        return TestFramework.getAllTestClasses();
+    }
+
+    public List<String> getAllFullQualifiedNameTestClasses() {
+        return this.getAllTestClasses().stream().map(CtType::getQualifiedName).collect(Collectors.toList());
+    }
+
+}
