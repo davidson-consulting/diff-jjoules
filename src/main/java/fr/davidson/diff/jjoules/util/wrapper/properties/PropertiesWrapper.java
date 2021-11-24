c582895c2be59addf75cad55897eed238dd91b71
danglotb <benjamin.danglot@davidson.fr>
2021-11-24 15:02:58 +0100

    style: remove unused import

--- src/main/java/fr/davidson/diff/jjoules/util/wrapper/properties/PropertiesWrapper.java
+++ src/main/java/fr/davidson/diff/jjoules/util/wrapper/properties/PropertiesWrapper.java
@@ -7,9 +7,6 @@ import spoon.Launcher;
 
 import java.io.File;
 import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
