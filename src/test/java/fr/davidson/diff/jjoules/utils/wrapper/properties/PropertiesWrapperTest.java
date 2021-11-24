b8c66ac590dd6c38846005bf98992277c0925f34
danglotb <benjamin.danglot@davidson.fr>
2021-11-24 15:35:12 +0100

    test: compute on the fly the classpath

--- src/test/java/fr/davidson/diff/jjoules/utils/wrapper/properties/PropertiesWrapperTest.java
+++ src/test/java/fr/davidson/diff/jjoules/utils/wrapper/properties/PropertiesWrapperTest.java
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,6 +26,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class PropertiesWrapperTest {
 
+    private final String[] CLASSPATH_ELEMENTS = new String[]{
+            "org/junit/jupiter/junit-jupiter-api/5.5.2/junit-jupiter-api-5.5.2.jar",
+            "org/apiguardian/apiguardian-api/1.1.0/apiguardian-api-1.1.0.jar",
+            "org/opentest4j/opentest4j/1.2.0/opentest4j-1.2.0.jar",
+            "org/junit/platform/junit-platform-commons/1.5.2/junit-platform-commons-1.5.2.jar",
+            "org/junit/jupiter/junit-jupiter-engine/5.5.2/junit-jupiter-engine-5.5.2.jar",
+            "org/junit/platform/junit-platform-engine/1.5.2/junit-platform-engine-1.5.2.jar",
+            "org/junit/platform/junit-platform-runner/1.3.2/junit-platform-runner-1.3.2.jar",
+            "org/junit/platform/junit-platform-launcher/1.3.2/junit-platform-launcher-1.3.2.jar",
+            "org/junit/platform/junit-platform-suite-api/1.3.2/junit-platform-suite-api-1.3.2.jar",
+            "junit/junit/4.12/junit-4.12.jar", "org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
+    };
+
     @BeforeEach
     void setUp() throws IOException {
         try {
@@ -31,6 +47,14 @@ public class PropertiesWrapperTest {
                     .forEach(File::delete);
         } catch (NoSuchFileException ignored) {
             // ignored
+        }
+        try (final FileWriter write = new FileWriter("src/test/resources/diff-jjoules-demo/classpath", false)) {
+            final String mavenHome = System.getProperty("user.home") + "/.m2/repository/";
+            write.write(
+                    Arrays.stream(CLASSPATH_ELEMENTS)
+                            .map(classpathElement -> mavenHome + Constants.FILE_SEPARATOR + classpathElement)
+                            .collect(Collectors.joining(Constants.PATH_SEPARATOR))
+            );
         }
     }
 
