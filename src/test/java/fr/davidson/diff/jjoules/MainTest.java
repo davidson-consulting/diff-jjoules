2d7efe35f9589037044a7c1aa7525ae28dbf0e4b
danglotb <benjamin.danglot@davidson.fr>
2021-11-24 17:11:10 +0100

    feat: add Main class

---
+++ src/test/java/fr/davidson/diff/jjoules/MainTest.java
@@ -0,0 +1,17 @@
+package fr.davidson.diff.jjoules;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * on 24/11/2021
+ */
+public class MainTest {
+
+    @Test
+    void testPrintUsage() {
+
+    }
+
+}
