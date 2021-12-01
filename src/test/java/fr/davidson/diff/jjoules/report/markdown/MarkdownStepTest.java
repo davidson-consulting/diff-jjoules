dc7f9031a14bf0fdd1536d2b239a92e50296934d
danglotb <benjamin.danglot@davidson.fr>
2021-12-01 13:43:47 +0100

    test: improve the test of MarkdownStep

--- src/test/java/fr/davidson/diff/jjoules/report/markdown/MarkdownStepTest.java
+++ src/test/java/fr/davidson/diff/jjoules/report/markdown/MarkdownStepTest.java
@@ -2,20 +2,20 @@ package fr.davidson.diff.jjoules.report.markdown;
 
 import fr.davidson.diff.jjoules.AbstractDiffJJoulesStepTest;
 import fr.davidson.diff.jjoules.Configuration;
+import fr.davidson.diff.jjoules.delta.Computation;
 import fr.davidson.diff.jjoules.delta.data.Data;
 import fr.davidson.diff.jjoules.delta.data.Datas;
 import fr.davidson.diff.jjoules.delta.data.Delta;
 import fr.davidson.diff.jjoules.delta.data.Deltas;
-import fr.davidson.diff.jjoules.util.Constants;
-import fr.davidson.diff.jjoules.util.JSONUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static fr.davidson.diff.jjoules.delta.MeasureEnergyConsumption.*;
+import static fr.davidson.diff.jjoules.util.Constants.NEW_LINE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -28,55 +28,110 @@ public class MarkdownStepTest extends AbstractDiffJJoulesStepTest {
 
     @Test
     void test() throws Exception{
+        final String testName2 = "fr.davidson.diff_jjoules_demo.InternalListTest#testCount2";
+        final String testName1 = "fr.davidson.diff_jjoules_demo.InternalListTest#testCount";
         final Configuration configuration = this.getConfiguration();
         final Deltas deltas = new Deltas();
         final Data dataV1 = new Data(10, 10, 10, 10, 10, 10, 10, 10);
         final Data dataV2 = new Data(100, 100, 100, 100, 100, 100, 100, 100);
         final Delta delta = new Delta(dataV1, dataV2);
-        deltas.put("fr.davidson.diff_jjoules_demo.InternalListTest#testCount", delta);
+
+        final Data dataV1Unconsidered1 = new Data(10, 10, 10, 10, 10, 10, 10, 10);
+        final Data dataV1Unconsidered2 = new Data(75, 75, 75, 75, 75, 75, 75, 75);
+        final Data dataV2Unconsidered1 = new Data(25, 25, 25, 25, 25, 25, 25, 25);
+        final Data dataV2Unconsidered2 = new Data(100, 100, 100, 100, 100, 100, 100, 100);
+        final Datas datasV1 = new Datas();
+
+        datasV1.put(testName1, Collections.singletonList(dataV1));
+        final List<Data> datasV1Unconsidered = new ArrayList<>();
+        datasV1Unconsidered.add(dataV1Unconsidered1);
+        datasV1Unconsidered.add(dataV1Unconsidered2);
+
+        datasV1.put(testName2, datasV1Unconsidered);
+        configuration.setDataV1(datasV1);
+
+        final Datas datasV2 = new Datas();
+        datasV2.put(testName1, Collections.singletonList(dataV2));
+        final List<Data> datasV2Unconsidered = new ArrayList<>();
+        datasV2Unconsidered.add(dataV2Unconsidered1);
+        datasV2Unconsidered.add(dataV2Unconsidered2);
+        datasV2.put(testName2, datasV2Unconsidered);
+        configuration.setDataV2(datasV2);
+
+        final Delta deltaUnconsidered = new Delta(
+                Computation.computeMedian(datasV1).get(testName2),
+                Computation.computeMedian(datasV2).get(testName2)
+        );
+        deltas.put(testName1, delta);
+        deltas.put(testName2, deltaUnconsidered);
         configuration.setDeltas(deltas);
         final Data deltaOmega = new Data(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5);
         configuration.setDeltaOmega(deltaOmega);
-        final Datas datasV1 = new Datas();
-        datasV1.put("fr.davidson.diff_jjoules_demo.InternalListTest#testCount", Collections.singletonList(dataV1));
-        final Datas datasV2 = new Datas();
-        datasV2.put("fr.davidson.diff_jjoules_demo.InternalListTest#testCount", Collections.singletonList(dataV2));
-        configuration.setDataV1(datasV1);
-        configuration.setDataV2(datasV2);
+
+        final Map<String, Long> report = new HashMap<>();
+        report.put(KEY_ENERGY_CONSUMPTION, 100000L);
+        report.put(KEY_INSTRUCTIONS, 100000L);
+        report.put(KEY_DURATIONS, 100000L);
+        configuration.addReport("ownConsumptionReport", report);
+        final Map<String, Double> scorePerLineV1 = new HashMap<>();
+        scorePerLineV1.put("fr.davidson.Example#exampleMethod#5", 0.5);
+        scorePerLineV1.put("fr.davidson.Example#exampleMethod#10", 1.0);
+        configuration.setScorePerLineV1(scorePerLineV1);
+        configuration.setScorePerLineV2(scorePerLineV1);
         new MarkdownStep()._run(configuration);
         try(final BufferedReader reader = new BufferedReader(new FileReader("target/report.md"))) {
-            assertEquals(EXPECTED_REPORT, reader.lines().collect(Collectors.joining(Constants.NEW_LINE)));
+            assertEquals(EXPECTED_REPORT, reader.lines().collect(Collectors.joining(NEW_LINE)));
         }
     }
 
-    private static final String EXPECTED_REPORT = "| Label | Consumption V1 | Consumption V2 | &Delta;(t) |\n" +
-            "| --- | --- | --- | --- |\n" +
-            "| fr.davidson.diff_jjoules_demo.InternalListTest |\n" +
-            "| testCount |\n" +
-            "| Energy(&mu;J) | 10.0 | 100.0 | 90.0 |\n" +
-            "| Instruction | 10.0 | 100.0 | 90.0 |\n" +
-            "| Duration(ms) | 10.0 | 100.0 | 90.0 |\n" +
-            "| Cycles | 10.0 | 100.0 | 90.0 |\n" +
-            "\n" +
-            "\n" +
-            "| Unit | &Delta;(&Omega;) | Raw&Delta; | Decision |\n" +
-            "| --- | --- | --- | --- |\n" +
-            "| Energy(&mu;J) | 0.5 | 90.0 | :x: |\n" +
-            "| Instructions | 0.5 | 90.0 | :x: |\n" +
-            "| Durations(ms) | 0.5 | 90.0 | :x: |\n" +
-            "| Cycles | 0.5 | 90.0 | :x: |\n" +
-            "\n" +
-            "\n" +
-            "## Suspicious Lines\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "## Diff-JJoules Consumption\n" +
-            "\n" +
-            "\n" +
-            "| Step | Energy(&mu;J) | Instruction | Durations(ms) |\n" +
-            "| --- | --- | --- | --- |\n" +
-            "| Diff-Jjoules | 0 | 0 | 0 |";
+    private static final String EXPECTED_REPORT = "| Label | Consumption V1 | Consumption V2 | &Delta;(t) |" + NEW_LINE +
+            "| --- | --- | --- | --- |" + NEW_LINE +
+            "| fr.davidson.diff_jjoules_demo.InternalListTest |" + NEW_LINE +
+            "| testCount |" + NEW_LINE +
+            "| Energy(&mu;J) | 10.0 | 100.0 | 90.0 |" + NEW_LINE +
+            "| Instruction | 10.0 | 100.0 | 90.0 |" + NEW_LINE +
+            "| Duration(ms) | 10.0 | 100.0 | 90.0 |" + NEW_LINE +
+            "| Cycles | 10.0 | 100.0 | 90.0 |" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "| Unit | &Delta;(&Omega;) | Raw&Delta; | Decision |" + NEW_LINE +
+            "| --- | --- | --- | --- |" + NEW_LINE +
+            "| Energy(&mu;J) | 0.5 | 90.0 | :x: |" + NEW_LINE +
+            "| Instructions | 0.5 | 90.0 | :x: |" + NEW_LINE +
+            "| Durations(ms) | 0.5 | 90.0 | :x: |" + NEW_LINE +
+            "| Cycles | 0.5 | 90.0 | :x: |" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "### Unconsidered Test Methods" + NEW_LINE +
+            "" + NEW_LINE +
+            "| Test | Delta | V1 | V2 |" + NEW_LINE +
+            "| --- | --- | --- | --- |" + NEW_LINE +
+            "| fr.davidson.diff_jjoules_demo.InternalListTest#testCount2 | 20.0 | 10.0,75.0 | 25.0,100.0 |" + NEW_LINE +
+            "## Suspicious Lines" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "### V1 and Deletions" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "| Line | Score |" + NEW_LINE +
+            "| --- | --- |" + NEW_LINE +
+            "| fr.davidson.Example#exampleMethod#10 | 1.0 |" + NEW_LINE +
+            "| fr.davidson.Example#exampleMethod#5 | 0.5 |" + NEW_LINE +
+            "### V2 and Additions" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "| Line | Score |" + NEW_LINE +
+            "| --- | --- |" + NEW_LINE +
+            "| fr.davidson.Example#exampleMethod#10 | 1.0 |" + NEW_LINE +
+            "| fr.davidson.Example#exampleMethod#5 | 0.5 |" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "## Diff-JJoules Consumption" + NEW_LINE +
+            "" + NEW_LINE +
+            "" + NEW_LINE +
+            "| Step | Energy(&mu;J) | Instruction | Durations(ms) |" + NEW_LINE +
+            "| --- | --- | --- | --- |" + NEW_LINE +
+            "| ownConsumptionReport | 100000 | 100000 | 100000 |" + NEW_LINE +
+            "| Diff-Jjoules | 100000 | 100000 | 100000 |";
 
 }
