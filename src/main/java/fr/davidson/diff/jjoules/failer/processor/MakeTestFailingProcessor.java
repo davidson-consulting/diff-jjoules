9f631593232ec24e2a7296e4c2ce39bd6a377307
danglotb <benjamin.danglot@davidson.fr>
2022-01-30 19:20:36 +0100

    fix: override processingDone to not add static block with TLPC

--- src/main/java/fr/davidson/diff/jjoules/failer/processor/MakeTestFailingProcessor.java
+++ src/main/java/fr/davidson/diff/jjoules/failer/processor/MakeTestFailingProcessor.java
@@ -1,13 +1,23 @@
 package fr.davidson.diff.jjoules.failer.processor;
 
 import fr.davidson.diff.jjoules.instrumentation.InstrumentationProcessor;
+import fr.davidson.diff.jjoules.util.Constants;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtTypeAccess;
+import spoon.reflect.declaration.CtAnonymousExecutable;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 import spoon.reflect.reference.CtExecutableReference;
 import spoon.reflect.reference.CtTypeReference;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,6 +27,8 @@ import java.util.Set;
  * on 30/06/2021
  */
 public class MakeTestFailingProcessor extends InstrumentationProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MakeTestFailingProcessor.class);
 
     public MakeTestFailingProcessor(Map<String, Set<String>> testsToBeInstrumented, String rootPathFolder, String testFolderPath) {
         super(testsToBeInstrumented, rootPathFolder, testFolderPath);
@@ -41,5 +53,10 @@ public class MakeTestFailingProcessor extends InstrumentationProcessor {
         this.instrumentedTypes.add(ctMethod.getDeclaringType());
     }
 
+    @Override
+    public void processingDone() {
+        this.instrumentedTypes.forEach(this::printCtType);
+        LOGGER.info("{} instrumented test classes have been printed!", this.instrumentedTypes.size());
+    }
 
 }
