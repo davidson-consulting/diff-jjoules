65ca754602c4dabe77639b496c2619d8c7bd8209
danglotb <benjamin.danglot@davidson.fr>
2022-02-04 19:00:56 +0100

    feat: method to join paths, files or lines

--- src/main/java/fr/davidson/diff/jjoules/util/Constants.java
+++ src/main/java/fr/davidson/diff/jjoules/util/Constants.java
@@ -1,5 +1,7 @@
 package fr.davidson.diff.jjoules.util;
 
+import java.util.List;
+
 /**
  * @author Benjamin DANGLOT
  * benjamin.danglot@davidson.fr
@@ -13,6 +15,29 @@ public class Constants {
 
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
-    public static final String CVS_SEPARATOR = ";";
+    public static String joinFiles(String... filenames) {
+        final String joinedFiles = String.join(FILE_SEPARATOR, filenames);
+        return joinedFiles.endsWith(FILE_SEPARATOR) ? joinedFiles : joinedFiles + FILE_SEPARATOR;
+    }
+
+    public static String joinFiles(List<String> filenames) {
+        return joinFiles(filenames.toArray(new String[0]));
+    }
+
+    public static String joinPaths(String... pathnames) {
+        return String.join(PATH_SEPARATOR, pathnames);
+    }
+
+    public static String joinPaths(List<String> pathnames) {
+        return joinPaths(pathnames.toArray(new String[0]));
+    }
+
+    public static String joinLines(String... lines) {
+        return String.join(NEW_LINE, lines);
+    }
+
+    public static String joinLines(List<String> lines) {
+        return joinLines(lines.toArray(new String[0]));
+    }
 
 }
