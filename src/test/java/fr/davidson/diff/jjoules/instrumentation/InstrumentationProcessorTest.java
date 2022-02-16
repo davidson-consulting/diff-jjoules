6bdd8a690fa25f9f49582089271973b6a99ed243
danglotb <benjamin.danglot@davidson.fr>
2022-02-16 11:47:31 +0100

    test: update InstrumentationProcessorTest

--- src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationProcessorTest.java
+++ src/test/java/fr/davidson/diff/jjoules/instrumentation/InstrumentationProcessorTest.java
@@ -1,10 +1,14 @@
 package fr.davidson.diff.jjoules.instrumentation;
 
 import fr.davidson.diff.jjoules.AbstractProcessorTest;
+import fr.davidson.diff.jjoules.util.Constants;
+import fr.davidson.diff.jjoules.util.FullQualifiedName;
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Benjamin DANGLOT
@@ -15,14 +19,26 @@ public class InstrumentationProcessorTest extends AbstractProcessorTest {
 
     @Override
     protected void oracle(CtMethod<?> testMethod) {
+        final String expectedIdentifier = new FullQualifiedName(TEST_CLASS_NAME, TEST_METHOD_NAME).toString();
         assertEquals(
-                "new fr.davidson.tlpc.sensor.TLPCSensor().start(\"" + TEST_METHOD_NAME + "\")",
+                "fr.davidson.tlpc.sensor.TLPCSensor.start(\"" + expectedIdentifier + "\")",
                 testMethod.getBody().getStatement(0).toString()
         );
         assertEquals(
-                "new fr.davidson.tlpc.sensor.TLPCSensor().stop(\"" + TEST_METHOD_NAME + "\")",
+                "fr.davidson.tlpc.sensor.TLPCSensor.stop(\"" + expectedIdentifier + "\")",
                 testMethod.getBody().getLastStatement().toString()
         );
+        final CtClass<?> ctClass = testMethod.getParent(CtClass.class);
+        final String expectedStaticBlock =
+                "static {" + Constants.NEW_LINE +
+                        "        java.lang.Runtime.getRuntime().addShutdownHook(new java.lang.Thread() {" + Constants.NEW_LINE +
+                        "            @java.lang.Override" + Constants.NEW_LINE +
+                        "            public void run() {" + Constants.NEW_LINE +
+                        "                fr.davidson.tlpc.sensor.TLPCSensor.report(\"target/trash//diff-jjoules-measurements/measurements.json/\");" + Constants.NEW_LINE +
+                        "            }" + Constants.NEW_LINE +
+                        "        });" + Constants.NEW_LINE +
+                        "    }";
+        assertTrue(ctClass.toString().contains(expectedStaticBlock), ctClass.toString());
     }
 
     @Override
