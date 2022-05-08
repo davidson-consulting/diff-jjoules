9baa4f729fa0dd5f702d3c8ec5e482605d7ee88b
danglotb <benjamin.danglot@davidson.fr>
2022-05-08 10:05:51 +0200

    feat: add method to compute the number total of covered lines

--- src/main/java/fr/davidson/diff/jjoules/selection/NewCoverage.java
+++ src/main/java/fr/davidson/diff/jjoules/selection/NewCoverage.java
@@ -10,4 +10,17 @@ import java.util.Map;
  * on 06/05/2022
  */
 public class NewCoverage extends HashMap<String, Map<String, Map<String, List<Integer>>>> {
+
+    public int getNbLineCovered() {
+        int nbLineCovered = 0;
+        for (String testFileName : this.keySet()) {
+            for (String testName : this.get(testFileName).keySet()) {
+                for (String sourceFileName : this.get(testFileName).get(testName).keySet()) {
+                    nbLineCovered += this.get(testFileName).get(testName).get(sourceFileName).size();
+                }
+            }
+        }
+        return nbLineCovered;
+    }
+
 }
